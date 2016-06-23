80110aeb057e992a23a606430221fbc1586d3062
L.T <ec.huyinghuan@gmail.com>
2016-06-23 13:00:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,23 @@
   };
   var s = false;
   
-  var target  = "长沙望城区驾考中心考场";
+  var target  = "星沙";
   
   setTimeout(function(){
-    school = $("#planDiv").text();
-    if("预约人数已满，稍等几分钟重试，等待别人放弃预约。"　!=  school){
-      if(school.indexOf(target) != -1){
-        showTips("可以预约考试");
-        s = true;
+      school = $("#planDiv").text();
+      //存在考场，但是不知道是不是目标考场
+      if( school.indexOf("预约人数已满") == -1){
+        //判断是否是目标考场
+        if(school.indexOf(target) != -1){
+          showTips("可以预约考试");
+        }else{
+          console.log("木有星沙考场")；
+          location.reload();
+        }
+      }else{
+        console.log("预约已满")；
+        location.reload();
       }
-    }
-  }, 3000);
-  
-  setTimeout(function(){
-    if(!s){location.reload();}
-  }, 7 * 1000);
-  
+      
+  }, 15 * 1000);
 })();
